89ba357338611d994d62e0e69f0eaa9393d29e26
dragosbucan <dragos.bucan@xivic.com>
2015-06-02 16:38:38 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+// Create a route handler for the home path
+app.get('/', function(req, res) {
+	res.render('index', {
+		title: 'My Page'
+	});
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Server listening at port ' + port);
